5ed04357b2b621498f8c4f74df384b953b6f83f6
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-03 19:26:05 +0600

    tech geeks with react router

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Login from './components/Login/Login';
+import Videos from './components/Videos/Videos';
+import NotFound from './components/NotFound/NotFound';
+import { createContext, useState } from 'react';
+import BlogDetails from './components/BlogDetails/BlogDetails';
+
+export const BlogContext = createContext();
+
+function App() {
+  const [blogs, setBlogs] = useState([])
+  return (
+    <BlogContext.Provider value = {[blogs, setBlogs]}>
+    <Navbar></Navbar>
+    <Routes>
+      <Route path = '/' element= {<Home></Home>}></Route>
+      <Route path = '/login' element = {<Login></Login>}></Route>
+      <Route path = '/videos' element = {<Videos></Videos>}></Route>
+      <Route path = '/blog/:id' element = {<BlogDetails></BlogDetails>}></Route>
+      <Route path = '*' element = {<NotFound></NotFound>}></Route>
+    </Routes>
+    </BlogContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/components/Videos/Videos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Videos = () => {
+    return (
+        <div>
+            <h3>This is videos page.</h3>
+        </div>
+    );
+};
+
+export default Videos;
