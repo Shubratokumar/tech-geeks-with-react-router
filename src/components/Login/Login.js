1efce7761f24f5f1042e8b3568014cffba7c11b3
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-13 18:04:35 +0600

    facebook and github auth added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./AuthForm.css";
 import GoogleLogo from "../../Assets/Image/google.svg";
+import FacebookLogo from "../../Assets/Image/Facebook.svg";
+import GitHubLogo from "../../Assets/Image/Github.svg";
 import { useNavigate } from "react-router-dom";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from './../../firebase.init';
 import toast from "react-hot-toast";
 
@@ -10,18 +12,48 @@ const Login = () => {
   const navigate = useNavigate();
 
   const googleProvider = new GoogleAuthProvider();
+  const facebookProvider = new FacebookAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () =>{
     signInWithPopup(auth, googleProvider)
     .then((result) =>{
       const user = result.user;
       navigate('/');
-      toast.success("Welcome!!! Successfully Logged In.")
+      toast.success("Welcome!!! Successfully Logged In with Google.")
       console.log(user);
     })
     .catch(error =>{
       toast.error("Opps!!! An error happend.")
       console.error(error);
+    })
+  }
+
+  const handlefacebookSignIn = () =>{
+    signInWithPopup(auth, facebookProvider)
+    .then((result) =>{
+      const user = result.user;
+      navigate("/")
+      toast.success("Welcome!!! Successfully Logged In with Facebook.")
+      console.log(user);
+    })
+    .catch((error) =>{
+      toast.error("Opps!!! An error happend.")
+      console.error(error);
+    })
+  }
+
+  const handleGitHubSignIn = () =>{
+    signInWithPopup(auth, gitHubProvider)
+    .then((result) =>{
+      const user = result.user;
+      navigate("/");
+      toast.success("Welcome!!! Successfully Logged In with GitHub.")
+      console.log(user);
+    })
+    .catch((error) =>{
+      toast.error("Opps!!! An error happend.")
+      console.error(error)
     })
   }
 
@@ -55,11 +87,25 @@ const Login = () => {
           <p>or</p>
           <div className='line-right' />
         </div>
-        <div className='input-wrapper'>
-          <button onClick={handleGoogleSignIn} className='google-auth'>
-            <img src={GoogleLogo} alt='' />
-            <p> Continue with Google </p>
-          </button>
+        <div className="social-container">
+          <div className='input-wrapper'>
+            <button onClick={handleGoogleSignIn} className='google-auth'>
+              <img src={GoogleLogo} alt='' />
+              <p> Continue with Google </p>
+            </button>
+          </div>
+          <div className='input-wrapper'>
+            <button onClick={handlefacebookSignIn} className='google-auth'>
+              <img src={FacebookLogo} alt='' />
+              <p> Continue with Facebook </p>
+            </button>
+          </div>
+          <div className='input-wrapper'>
+            <button onClick={handleGitHubSignIn} className='google-auth'>
+              <img src={GitHubLogo} alt='' />
+              <p> Continue with GitHub </p>
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import GoogleLogo from "../../Assets/Image/google.svg";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import FacebookLogo from "../../Assets/Image/Facebook.svg";
+import GitHubLogo from "../../Assets/Image/Github.svg";
+import { FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from './../../firebase.init';
 import toast from "react-hot-toast";
 
@@ -9,6 +11,8 @@ const Signup = () => {
   const navigate = useNavigate();
 
   const googleProvider = new GoogleAuthProvider();
+  const facebookProvider = new FacebookAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () =>{
     signInWithPopup(auth, googleProvider)
@@ -24,6 +28,34 @@ const Signup = () => {
     })
   }
 
+  const handlefacebookSignIn = () =>{
+    signInWithPopup(auth, facebookProvider)
+    .then((result) =>{
+      const user = result.user;
+      navigate("/")
+      toast.success("Welcome!!! Successfully Logged In with Facebook.")
+      console.log(user);
+    })
+    .catch((error) =>{
+      toast.error("Opps!!! An error happend.")
+      console.error(error);
+    })
+  }
+
+  const handleGitHubSignIn = () =>{
+    signInWithPopup(auth, gitHubProvider)
+    .then((result) =>{
+      const user = result.user;
+      navigate("/");
+      toast.success("Welcome!!! Successfully Logged In with GitHub.")
+      console.log(user);
+    })
+    .catch((error) =>{
+      toast.error("Opps!!! An error happend.")
+      console.error(error)
+    })
+  
+  }
   return (
     <div className='auth-form-container '>
       <div className='auth-form'>
@@ -64,11 +96,25 @@ const Signup = () => {
           <p>or</p>
           <div className='line-right' />
         </div>
-        <div className='input-wrapper'>
-          <button onClick={handleGoogleSignIn} className='google-auth'>
-            <img src={GoogleLogo} alt='' />
-            <p> Continue with Google </p>
-          </button>
+        <div className="social-container">
+          <div className='input-wrapper'>
+            <button onClick={handleGoogleSignIn} className='google-auth'>
+              <img src={GoogleLogo} alt='' />
+              <p> Continue with Google </p>
+            </button>
+          </div>
+          <div className='input-wrapper'>
+            <button onClick={handlefacebookSignIn} className='google-auth'>
+              <img src={FacebookLogo} alt='' />
+              <p> Continue with Facebook </p>
+            </button>
+          </div>
+          <div className='input-wrapper'>
+            <button onClick={handleGitHubSignIn} className='google-auth'>
+              <img src={GitHubLogo} alt='' />
+              <p> Continue with GitHub </p>
+            </button>
+          </div>
         </div>
       </div>
     </div>
